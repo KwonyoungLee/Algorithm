293d993cec7ee0a83a3384cc1e72a7ac6fcefd4e
KwonyoungLee <55869067+KwonyoungLee@users.noreply.github.com>
2021-06-20 16:48:47 -0500

    Update 10_02_is_palindrome.py

--- Linked_List_and_Array/10_02_is_palindrome.py
+++ Linked_List_and_Array/10_02_is_palindrome.py
@@ -3,7 +3,9 @@ input = "abcba"
 #abcba
 #bcb
 #c
-
+# 재귀함수를 풀기 위해선 
+# 문제가 축소되는 특징이 보여야 합니다.
+# 특정 구조가 반복되는 것 같은 양상을 보였으면 재귀함수로 시도 가능.
 def is_palindrome(string):
     # input 의 길이가 1이거나 작다면
     if len(string) <= 1:
